dfcb09f341fe75b8ed4967fbc980ee37e93a7dd5
OmerJalal1 <zoneomer3@gmail.com>
2026-04-24 20:02:21 +0300

    Add CSRF_TRUSTED_ORIGINS

--- buses_project/settings.py
+++ buses_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-s(p4@48*-i&*9ycsuy(i#aztki
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = ['web-production-695a2.up.railway.app', 'localhost', '127.0.0.1']
-
+CSRF_TRUSTED_ORIGINS = ['https://web-production-695a2.up.railway.app']
 
 # Application definition
 
